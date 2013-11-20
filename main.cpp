ad4882cbb2a1fd1442d120a0dca48798d38c6034
Kavan Sikand <kavanthewise@yahoo.com>
2013-11-19 16:17:37 -0800

    added code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  final
+//
+//  Created by Kavan Sikand on 11/19/13.
+//  Copyright (c) 2013 Kavan Sikand. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
